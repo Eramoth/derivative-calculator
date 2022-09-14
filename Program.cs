e21b7a2e1802f2b417a53144ffc58b9ad772adcf
AREFYTON <axelle.refyton@ynov.com>
2022-09-14 11:35:16 +0200

    hello world!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DerivatoryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
